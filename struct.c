e4ae4bf89e477ba28ffa9b40ba1001b1039de01d
lmr lumiere <126115024+Lumiere-MULAGWA@users.noreply.github.com>
2024-06-24 00:01:58 +0200

    Update struct.c

--- struct.c
+++ struct.c
@@ -4,13 +4,14 @@ typedef struct
 {
     int a;
     int b;
+    int c;
 } Donnee;
 typedef int Entier;
 
 int main (){
 
     Donnee varDonnee = {
-        .a =5 ,.b =9
+        .a =5 ,.b =9,.c=12
     };
     Donnee *ptr = &varDonnee;
     printf("Structure :.A = %d , .B = %d \n",varDonnee.a,varDonnee.b);
