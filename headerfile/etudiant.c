e3f56b92a81e109f658ade427e7c193dc5889dbd
lmr lumiere <126115024+Lumiere-MULAGWA@users.noreply.github.com>
2024-06-24 21:12:22 +0200

    add

---
+++ headerfile/etudiant.c
@@ -0,0 +1,62 @@
+#include "etudiant.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void afficherEtudiant (Etudiant *);
+void modifierEtudiant (Etudiant *,char *nom,char *postNom,
+                         char *prenom,char genre,Date dateNaiss);
+
+
+/**
+ * Fonction permettant de creer des etudiants 
+ * allouer la memoire pour un etudiant et renvoyer le pointeur
+*/
+Etudiant * creerEtudiant(char *matricule,char *nom,char *postNom,
+                         char *prenom,char genre,Date dateNaiss)
+{
+    Etudiant * etu = malloc(sizeof(Etudiant));
+    if (etu != NULL){
+        strcpy(etu->matricule,matricule);
+        strcpy(etu->nom,nom);
+        strcpy(etu->postNom,postNom);
+        strcpy(etu->prenom,prenom);
+        etu->genre = genre;
+        etu->dateNaiss = dateNaiss;
+        etu ->afficher = afficherEtudiant;
+        etu->modifier = modifierEtudiant;
+    }
+return etu;
+
+                        
+}
+
+void afficherEtudiant (Etudiant * etu)
+{
+    printf("matricule = %s Nom = %s  prenom = %s  postnom = %s genre = %s  date = %d/%d/%d ",
+    etu->matricule,
+    etu->nom, 
+    etu->prenom,
+    etu->postNom, 
+    etu->genre,
+    etu->dateNaiss.jour, 
+    etu->dateNaiss.mois, 
+    etu->dateNaiss.annee);
+}
+void modifierEtudiant (Etudiant * etu,char *nom,char *postNom,
+                         char *prenom,char genre,Date dateNaiss){
+   
+    if(etu != NULL){
+        strcpy(etu->nom,nom);
+        strcpy(etu->postNom,postNom);
+        strcpy(etu->prenom,prenom);
+        etu->genre = genre;
+        etu->dateNaiss = dateNaiss;
+    }
+    
+    
+
+return true;
+
+}
