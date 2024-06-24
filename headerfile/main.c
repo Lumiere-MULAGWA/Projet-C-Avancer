2062f3d04e33a81270cb620b4121b6cb8554e06f
lmr lumiere <126115024+Lumiere-MULAGWA@users.noreply.github.com>
2024-06-24 21:13:14 +0200

    add main

---
+++ headerfile/main.c
@@ -0,0 +1,17 @@
+#include "etudiant.h"
+#include <stdio.h>
+
+int main (){
+    char * matricule = "21mb359";
+    char * nom = "lmr";
+    char * postNom = "lum";
+    char * prenom = "lucifer";
+    char genre = "M";
+    Date dateN = {.jour = 5,.mois = 5 ,.annee = 2010};
+
+    Etudiant * etudiant1 = creerEtudiant(matricule,nom,postNom,prenom,genre,dateN);
+    etudiant1 -> afficher(etudiant1);
+
+    free(etudiant1);    
+    return 0;
+}
